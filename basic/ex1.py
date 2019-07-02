0c8f3f3994930959cc9dcef9f01e303247719175
lkogler <lkogler@tuwien.ac.at>
2019-07-02 08:34:21 +0200

    oops, deleted examples by accident

---
+++ basic/ex1.py
@@ -0,0 +1,56 @@
+
+# EX 1: super simple poisson
+
+from ngsolve import *
+
+ngsglobals.msg_level = 1
+
+# get handle to the MPI communicator
+comm = mpi_world
+
+print("Hello from rank", comm.rank, "of", comm.size)
+
+if comm.rank == 0:
+    # the master-proc generates mesh
+    from netgen.geom2d import unit_square
+    mesh = unit_square.GenerateMesh(maxh=0.1)
+    ngsmesh = Mesh(mesh)
+    print("global NV =", ngsmesh.nv, ", and NE =", ngsmesh.ne)
+    if comm.size > 1:
+        mesh.Distribute(comm)
+else:
+    # the others wait to receive their part of the mesh
+    # from the master
+    from netgen.meshing import Mesh as NGMesh
+    mesh = NGMesh.Receive(comm)
+mesh = Mesh(mesh)
+
+print("rank", comm.rank, ", local NV =", mesh.nv, ", and NE =", mesh.ne)
+
+# build H1-FESpace as usual
+V = H1(mesh, order=3, dirichlet=".*")
+
+print("rank", comm.rank, "has", V.ndof, "of", V.ndofglobal, "dofs!")
+
+u,v = V.TnT()
+
+# RHS does not change either!
+f = LinearForm (V)
+f += 32 * (y*(1-y)+x*(1-x)) * v * dx
+f.Assemble()
+
+# neither does the BLF!
+a = BilinearForm (V)
+a += grad(u) * grad(v) * dx
+
+gfu = GridFunction(V)
+
+## CG + Jacobi Preconditioner
+c = Preconditioner(a, type="local")
+a.Assemble()
+
+
+solvers.CG(mat=a.mat, pre=c.mat, sol=gfu.vec, rhs=f.vec, maxsteps=100,
+           tol=1e-6, printrates=comm.rank==0) # !
+
+gfu.Save("ex1.sol", parallel=True)
---
+++ basic/ex5.py
@@ -0,0 +1,73 @@
+
+from ngsolve import *
+
+comm = mpi_world
+
+if comm.rank==0:
+    from netgen.geom2d import unit_square
+    from netgen.geom2d import *
+    geo = SplineGeometry()
+    pnts = [ (0,0), (1,0), (1,1), (0,1) ]
+    pnums = [geo.AppendPoint(*p) for p in pnts]
+    lbot = geo.Append ( ["line", pnums[0], pnums[1]],bc="bot")
+    # geo.Append ( ["line", pnums[3], pnums[2]], leftdomain=0, rightdomain=1, bc="top")
+    geo.Append ( ["line", pnums[3], pnums[2]], leftdomain=0, rightdomain=1, copy=lbot, bc="top")
+    lright = geo.Append ( ["line", pnums[1], pnums[2]], bc="right")
+    geo.Append ( ["line", pnums[0], pnums[3]], leftdomain=0, rightdomain=1, copy=lright, bc="left")
+    ngmesh = geo.GenerateMesh(maxh=0.05)
+    if comm.size>1:
+        ngmesh.Distribute(comm)
+else:
+    from netgen.meshing import Mesh as NGMesh
+    ngmesh = NGMesh.Receive(comm)
+mesh = Mesh(ngmesh)
+
+V = L2(mesh, order=4)
+
+u,v = V.TnT()
+
+# b = CoefficientFunction( (y-0.5,0.5-x) )
+b = CoefficientFunction( (0.3 + y-0.5,0.5-x) )
+bn = b*specialcf.normal(2)
+
+a = BilinearForm(V)
+a += SymbolicBFI (-u * b*grad(v))
+a += SymbolicBFI ( bn*IfPos(bn, u, u.Other()) * (v-v.Other()), VOL, skeleton=True)
+a += SymbolicBFI ( bn*IfPos(bn, u, 0) * v, BND, skeleton=True)
+# a += u * b*grad(v) * dx
+# a += bn*IfPos(bn, u, u.Other()) * (v-v.Other()) * dx(skeleton=True)
+# a += bn*IfPos(bn, u, 0) * dx(skeleton=True)
+
+u = GridFunction(V)
+u.Set(exp (-40 * ( (x-0.7)*(x-0.7) + (y-0.7)*(y-0.7) )))
+
+w = u.vec.CreateVector()
+
+t = 0
+tau = 5e-4
+tend = 10
+count = 0
+out_interval = 0.02 // tau
+
+import os
+output_path = os.path.dirname(os.path.realpath(__file__)) + "/solutions"
+if comm.rank == 0 and not os.path.exists(output_path):
+    os.mkdir(output_path)
+comm.Barrier()
+
+Draw(u, name="sol")
+
+with TaskManager():
+    while t < tend:
+        if comm.rank == 0:
+            print("\rt =", t, ", # of files =", int(count/out_interval), end=' ')
+        a.Apply (u.vec, w)
+        V.SolveM (rho=CoefficientFunction(1), vec=w)
+        u.vec.data -= tau * w
+        t += tau
+        if count % out_interval == 0:
+            u.Save(output_path + '/u_' + str(int(count/out_interval)) + '.sol', parallel=True)
+        count = count+1;
+comm.Barrier()
+if comm.rank == 0:
+    print('')
